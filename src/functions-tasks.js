8bf533946fda248f1cd7599f716205e2a2e3b6ef
Dzmitry <d.prankevich@gmail.com>
2025-04-13 00:42:48 +0300

    feat: add solution 6th task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -114,8 +114,17 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache = null;
+  let hascache = false;
+
+  return function f() {
+    if (!hascache) {
+      cache = func();
+      hascache = true;
+    }
+    return cache;
+  };
 }
 
 /**
